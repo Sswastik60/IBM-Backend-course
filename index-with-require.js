bc3dd9731cc99578be85d350696a394e1bb316bb
Sswastik60 <sswastik60@gmail.com>
2026-07-10 23:12:08 +0530

    lab1.1

---
+++ index-with-require.js
@@ -0,0 +1,45 @@
+ // Import the HTTP module
+const http = require('http');
+
+// Import the 'today' module
+const today = require('./today');
+
+
+// Define the request listener function
+const requestListener = function (req, res) {
+    res.writeHead(200); // Set the status code to 200 (OK)
+    let dateVal = today.getDate();
+    console.log(dateVal);
+    console.log(dateVal.getHours()); // Get the current date from the 'today' module
+
+    // Determine the appropriate greeting based on the current time
+    const hour = dateVal.getHours();
+
+    let greeting;
+
+    if (hour >= 6 && hour < 12) {
+        greeting = "Good morning!";
+    } else if (hour >= 12 && hour < 18) {
+        greeting = "Good afternoon!";
+    } else if (hour >= 18 && hour < 21) {
+        greeting = "Good evening!";
+    } else {
+        greeting = "Good night!";
+    }
+
+    res.end(`Hello, ${greeting}`);
+    
+
+    // Send the response with the appropriate greeting
+    res.end(`Hello, ${greeting}`);
+};
+
+// Define the port number
+const port = 3000;
+
+// Create an HTTP server using the request listener function
+const server = http.createServer(requestListener);
+
+// Start the server and listen on the specified port
+server.listen(port);
+console.log('Server listening on port: ' + port);
